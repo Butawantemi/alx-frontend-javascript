9d2a31da8cee7df9a2e53afe836e34d52aab5767
Butawantemi <japhetbuta@gmail.com>
2024-08-13 20:19:01 +0300

    promise

--- 0x01-ES6_promise/promise.js
+++ 0x01-ES6_promise/promise.js
@@ -2,5 +2,7 @@ var promise = new Promise(function(resolve, reject) {
     // do a thing, possubly async, then...
     if (/* everything turned out fine */){
         resolve("Stuff worked!")
-    }else{}
+    }else{
+        reject(Error("It broke"))
+    }
 })
