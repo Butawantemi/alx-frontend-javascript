3406dcd3bdea9163fbf84b891793b7fa2cd7ee04
Butawantemi <japhetbuta@gmail.com>
2024-08-13 21:26:17 +0300

    promise

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,8 @@
+function getResponseFromAPI(apiEndPoint){
+    return new Promise((resolve, reject) =>{
+        if (apiEndPoint){
+            resolve({status: 200, body: 'Success'});
+            reject(new Error('Failed to fetch the API'));
+        }
+    })
+}
