a8c2121afdef2903d97a6c6642e307226168848e
Butawantemi <japhetbuta@gmail.com>
2024-08-13 20:22:51 +0300

    promise

--- 0x01-ES6_promise/promise.js
+++ 0x01-ES6_promise/promise.js
@@ -1,8 +1,15 @@
 var promise = new Promise(function(resolve, reject) {
     // do a thing, possubly async, then...
-    if (/* everything turned out fine */){
+    if (/* everything turned out fine */ TRUE){
         resolve("Stuff worked!")
     }else{
         reject(Error("It broke"))
     }
+})
+
+//How to use promise
+promise.then(function(result){
+    console.log(result)//"Stuff worked!"
+}, function(err){
+    console.log(err)//"It broke"
 })
