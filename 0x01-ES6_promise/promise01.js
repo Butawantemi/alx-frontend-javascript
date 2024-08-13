8cf68465e6bddac174c79f7a1e0f6d137cf5d823
Butawantemi <japhetbuta@gmail.com>
2024-08-13 20:44:39 +0300

    promise

--- 0x01-ES6_promise/promise01.js
+++ 0x01-ES6_promise/promise01.js
@@ -1,4 +1,9 @@
-var promise = new promise(function(resolve, reject){
+var promise = new Promise(function(resolve, reject){
     resolve(1);
 })
 
+promise.then(function(val){
+    return val + 2;
+}).then(function(val){
+    return val + 3;
+})
