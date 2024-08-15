98eec09e5407d56cb19d8cb0ff32eef023f00440
Butawantemi <japhetbuta@gmail.com>
2024-08-15 10:17:52 +0300

    promise

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,3 +1,10 @@
+import { uploadPhoto, createUser } from './utils.js';
+
 export default function handleProfileSignup() {
-    
+  const promises = [uploadPhoto(), createUser()];
+
+  return Promise.all(promises).then((data) => {
+    const [photoData, userData] = data;
+    console.log(`${photoData.body} ${userData.firstName} ${userData.lastName}`);
+  }).catch(() => console.log('Signup system offline'));
 }
