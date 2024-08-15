f6b3aacd10f68a01243cfcec82015f950fc59c12
Butawantemi <japhetbuta@gmail.com>
2024-08-15 09:29:27 +0300

    promise

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,15 @@
+function handleResponseFromAPI(){
+    return new Promise((resolve, reject) =>{
+        resolve(
+            {
+                status: 200,
+                body: 'Successs'
+            }
+        );
+        console.log('Got a response from the API');
+        reject(
+            new Error()
+        )
+    });
+    
+}
