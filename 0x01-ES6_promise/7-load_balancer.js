b72709de15f260cfc0b98f837f48c6355256b4dd
Butawantemi <japhetbuta@gmail.com>
2024-08-17 19:01:21 +0300

    promise

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -1,6 +1,3 @@
 export default function loadBalancer(chinaDownload, USDownload) {
-     return Promise.race([chinaDownload, USDownload]).then((value) => {
-        value
-     });
-    
-}
+    return Promise.race([chinaDownload, USDownload]).then((value) => value);
+  }
