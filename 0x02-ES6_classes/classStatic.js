ff37f4544a2e18357405482160312b03703cc099
Butawantemi <japhetbuta@gmail.com>
2024-08-20 19:51:18 +0300

    classes

--- 0x02-ES6_classes/classStatic.js
+++ 0x02-ES6_classes/classStatic.js
@@ -7,5 +7,11 @@ class Point {
     static distance(a, b){
         const dx = a.x - b.x;
         const dy = a.y - b.y;
+
+        return Math.hypot(dx, dy);
     }
+
 }
+
+const p1 = new Point(5, 5);
+const p2 = new Point(10, 10);
