d06351c7d2ce8c368c5a380599abad6bebfca1ac
Butawantemi <japhetbuta@gmail.com>
2024-08-20 20:41:38 +0300

    classes

---
+++ 0x02-ES6_classes/classInherit2.js
@@ -0,0 +1,12 @@
+class Cat {
+    constructor(name){
+        this.name = name;
+    }
+    Speak(){
+        console.log(this.name + ' makes a noise.');
+    }
+}
+
+class Lion extends Cat {
+    
+}
