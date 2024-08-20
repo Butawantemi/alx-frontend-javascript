c8ebaed736ccbd9fe8a43df7836578ea872ca1a8
Butawantemi <japhetbuta@gmail.com>
2024-08-20 19:18:10 +0300

    classes

--- 0x02-ES6_classes/class.js
+++ 0x02-ES6_classes/class.js
@@ -1,4 +1,13 @@
+//Declaration of a class
 class Rectangle {
+    constructor(height, width) {
+      this.height = height;
+      this.width = width;
+    }
+  }
+
+  //Expression of a class
+  let Rectangle = class {
     constructor(height, width) {
       this.height = height;
       this.width = width;
