a9dc17c41583de7e5b56a8a2171807e0e8a51061
Butawantemi <japhetbuta@gmail.com>
2024-08-20 20:12:12 +0300

    classes

--- 0x02-ES6_classes/classInherit.js
+++ 0x02-ES6_classes/classInherit.js
@@ -1,14 +1,17 @@
+//Declaring a class and extending it with another class
 class Animal {
     constructor(name) {
         this.name = name;
 
     }
-
+    
+    //Method
     Speak(){
         console.log(this.name + ' makes a noise.');
     }
 }
 
+//Extending the Animal class
 class Mouse extends Animal {
     constructor(name){
         super(name);
@@ -19,6 +22,10 @@ class Mouse extends Animal {
     }
 }
 
+//Creating instances of the classes
 let animal =  new Animal('Animal');
 let mouse = new Mouse('Mouse');
 
+//Calling the Speak method
+animal.Speak();
+mouse.Speak();
