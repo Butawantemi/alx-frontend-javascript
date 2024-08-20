623a3e6151ef3eb751adc4050090c28094934e10
Butawantemi <japhetbuta@gmail.com>
2024-08-20 19:56:15 +0300

    classes

--- 0x02-ES6_classes/classStatic.js
+++ 0x02-ES6_classes/classStatic.js
@@ -1,8 +1,10 @@
+//Declare a class named Point
 class Point {
     constructor(x, y){
         this.x = x;
         this.y = y;
     }
+    //static method
     static displayName = 'Point';
     static distance(a, b){
         const dx = a.x - b.x;
@@ -13,8 +15,10 @@ class Point {
 
 }
 
+//Create two instances of Point
 const p1 = new Point(5, 5);
 const p2 = new Point(10, 10);
 
+//Display name property
 console.log(Point.displayName);
 console.log(Point.distance(p1, p2));
