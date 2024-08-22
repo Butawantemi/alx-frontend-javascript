cf9930ad2a5e3a1d53ca0220f2859d373fa357ea
Butawantemi <japhetbuta@gmail.com>
2024-08-22 20:14:00 +0300

    classes

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,8 @@
+export default class Building {
+    constructor(sqft){
+        if (typeof sqft !== "number") {
+            throw new Error("Sqft must be a number");
+        }
+        this._sqft = sqft;
+    }
+}
