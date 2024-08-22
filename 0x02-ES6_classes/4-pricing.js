f4dea2dd7553083c0289ec02da2daad691004c07
Butawantemi <japhetbuta@gmail.com>
2024-08-22 19:52:35 +0300

    classes

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -9,13 +9,25 @@ export default class pricing {
     this._currency = currency;
   }
 
-   get amount(){
+  get amount() {
     return this._amount;
-   }
+  }
 
-   set amount(value) {
-    if (typeof value !== "number"){
-        throw new Error("Amount must be a number");
+  set amount(value) {
+    if (typeof value !== "number") {
+      throw new Error("Amount must be a number");
     }
-   }
+    this._amount = value;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    if (!(value instanceof Currency)) {
+      throw new Error("Currency must be a Currency");
+    }
+    this._currency = value;
+  }
 }
