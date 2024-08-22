3bbf5013a28f9b5410448f66950eae0f14f314f3
Butawantemi <japhetbuta@gmail.com>
2024-08-22 21:03:06 +0300

    classes

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -6,4 +6,12 @@ export default class Airport {
     this._name = name;
     this._code = code;
   }
+
+   get name(){
+    return this._name;
+   }
+
+   set name(value){
+    this._name = value;
+   }
 }
