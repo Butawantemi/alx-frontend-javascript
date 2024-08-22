4f7593e53b88632cb0257e30fd251b46fa084eeb
Butawantemi <japhetbuta@gmail.com>
2024-08-22 19:09:13 +0300

    classes

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,11 +1,11 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
     if (
-      typeof name !== 'string' ||
-      typeof length !== 'number' ||
+      typeof name !== "string" ||
+      typeof length !== "number" ||
       !Array.isArray(students)
     ) {
-      throw new Error('Invalid type');
+      throw new Error("Invalid type");
     }
     this._name = name;
     this._length = lenght;
@@ -17,9 +17,31 @@ export default class HolbertonCourse {
   }
 
   set name(value) {
-    if (typeof value !== 'string') {
-      throw new Error('Name must be a string');
+    if (typeof value !== "string") {
+      throw new Error("Name must be a string");
     }
     this._name = value;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(value) {
+    if (typeof value !== "number") {
+      throw new Error("Length must be a number");
+    }
+    this._length = value;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(value) {
+    if (!Array.isArray(value)) {
+      throw new Error("Students must be an array");
+    }
+    this._students = value;
   }
 }
