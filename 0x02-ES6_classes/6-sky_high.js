ba09f6c910dad368324e8c841900e9b666e5874a
Butawantemi <japhetbuta@gmail.com>
2024-08-22 20:49:42 +0300

    classes

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,8 +1,28 @@
 import Building from "./5-building.js";
 
 export default class SkyHighBuilding extends Building {
-    constructor(sqft, floors){
-        super(sqft);
-        this.floors = floors;
-    }
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(value) {
+    this._floors = value;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    this._sqft = value;
+  }
+
+  evacuationWarningMessage() {
+    return "Evacuate slowly the building";
+  }
 }
