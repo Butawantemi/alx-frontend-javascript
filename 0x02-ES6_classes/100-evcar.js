1a4a4613d42f954d1b8f122e5cb902da29d97611
Butawantemi <japhetbuta@gmail.com>
2024-08-23 19:57:59 +0300

    classes

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,20 +1,21 @@
 import Car from "./10-car.js";
 
 export default class EVCar extends Car {
-    constructor(brand, motor, color, range){
+    constructor(brand, motor, color, range) {
         super(brand, motor, color);
         this._range = range;
     }
 
-    get range(){
+    get range() {
         return this._range;
     }
 
-    set range(value){
+    set range(value) {
         this._range = value;
     }
 
-    cloneCar(){
-        return new this.constructor(this._brand, this._motor, this.color, this._range);
+    cloneCar() {
+        // Create a new instance of the Car class with the brand, motor, and color
+        return new Car(this._brand, this._motor, this._color);
     }
 }
