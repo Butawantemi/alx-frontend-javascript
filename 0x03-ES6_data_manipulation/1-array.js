16616159048c995bc7911328350731fe7c47a323
Butawantemi <japhetbuta@gmail.com>
2024-08-25 19:58:16 +0300

    Data manipulation

--- 0x03-ES6_data_manipulation/1-array.js
+++ 0x03-ES6_data_manipulation/1-array.js
@@ -1,5 +1,5 @@
 const colors = ['red', 'green', 'blue', 'yellow', 'black'];
-
+colors[6] = 'white';
 const iterator = colors.keys();
 for (const key of iterator) {
   console.log(`${key} - ${colors[key]}`);
