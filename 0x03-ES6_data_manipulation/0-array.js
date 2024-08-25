f5da8758e3a63363eafa39e109387ab7f230a346
Butawantemi <japhetbuta@gmail.com>
2024-08-25 19:48:21 +0300

    Data manipulation

--- 0x03-ES6_data_manipulation/0-array.js
+++ 0x03-ES6_data_manipulation/0-array.js
@@ -1,3 +1,8 @@
-let fruit = ['manzana', 'banana', 'pera', 'uva', 'sandia'];
+const fruit = ['manzana', 'banana', 'pera', 'uva', 'sandia'];
 
-console.log(Object.keys(fruit));
+// console.log(Object.keys(fruit));
+
+// loop through the array and print all the elements
+fruit.forEach((item, index) => {
+    console.log(`Index: ${index} - Item: ${item}`);
+});
