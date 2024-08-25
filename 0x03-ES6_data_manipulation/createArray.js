4df503f06c28354c82725c70898a58d81ba1bc52
Butawantemi <japhetbuta@gmail.com>
2024-08-25 20:40:44 +0300

    Data manipulation

--- 0x03-ES6_data_manipulation/createArray.js
+++ 0x03-ES6_data_manipulation/createArray.js
@@ -2,3 +2,8 @@
 const colors = ['red', 'green', 'blue'];
 
 console.log(colors.length);
+
+// Creating an array using the Array() constructor.
+const fruit2 = new Array('apple', 'banana', 'pear');
+
+console.log(fruit2.length);
