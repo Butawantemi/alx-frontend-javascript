87fe6cfba87d4e87d26fcf348deb706a1c656454
Butawantemi <japhetbuta@gmail.com>
2024-08-26 19:44:24 +0300

    Data manipulation

--- 0x03-ES6_data_manipulation/15-array.js
+++ 0x03-ES6_data_manipulation/15-array.js
@@ -3,3 +3,8 @@ const brands = ['Apple', 'Samsung', 'Huawei'];
 
 // Copy an array using the spread operator.
 const copyBrands = [...brands];
+
+// Copy an array using the slice method.
+const copyBrands2 = brands.slice();
+
+// 
