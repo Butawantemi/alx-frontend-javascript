e733536ffba8169a459208c24fdb51ed829bf6b6
Butawantemi <japhetbuta@gmail.com>
2024-08-26 18:07:24 +0300

    Data manipulation

--- 0x03-ES6_data_manipulation/4-array.js
+++ 0x03-ES6_data_manipulation/4-array.js
@@ -1,7 +1,9 @@
 // An Array
 const city = ['Dodoma', 'Dar es Salaam', 'Mwanza'];
 
+// Add an element to the end of an array.
 const newLength = city.push('Arusha');
 
+// Output the array and its length.
 console.log(city);
 console.log(newLength);
