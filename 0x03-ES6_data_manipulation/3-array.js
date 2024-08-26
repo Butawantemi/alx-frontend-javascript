4debee127e66a3b460b5dfb51b1ebda6e105118d
Butawantemi <japhetbuta@gmail.com>
2024-08-26 17:59:52 +0300

    Data manipulation

--- 0x03-ES6_data_manipulation/3-array.js
+++ 0x03-ES6_data_manipulation/3-array.js
@@ -1,6 +1,9 @@
+// An array.
 const colors = ['red', 'green', 'blue'];
 
+// Check if an array includes a certain element.
 console.log(colors.includes('red'));
-console.log(colors.indexOf('yellow'));
+console.log(colors.includes('yellow'));
 
+// Find the index of an element in an array.
 console.log(colors.indexOf('red'));
