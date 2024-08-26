bb41d882ece583f5ded1b0a70de39044918d97cf
Butawantemi <japhetbuta@gmail.com>
2024-08-26 19:17:45 +0300

    Data manipulation

--- 0x03-ES6_data_manipulation/13-array.js
+++ 0x03-ES6_data_manipulation/13-array.js
@@ -2,3 +2,6 @@
 const cars = ['BMW', 'Audi', 'Mercedes', 'Toyota'];
 
 // Iterate over the array and output each elements.
+cars.forEach((item, index) => {
+  console.log(`${index} : ${item}`);
+});
