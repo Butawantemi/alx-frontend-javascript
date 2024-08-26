15be8222cd545d4866c731985f912111c83b9785
Butawantemi <japhetbuta@gmail.com>
2024-08-26 19:49:44 +0300

    Data manipulation

--- 0x03-ES6_data_manipulation/15-array.js
+++ 0x03-ES6_data_manipulation/15-array.js
@@ -10,6 +10,9 @@ const copyBrands2 = brands.slice();
 // Copy an array using the Array.from method.
 const copyBrands3 = Array.from(brands);
 
+// Copy an array using JSON.stringify and JSON.parse.
+const copyBrands4 = JSON.parse(JSON.stringify(brands));
+
 // Output
 console.log(copyBrands);
 console.log(copyBrands2);
