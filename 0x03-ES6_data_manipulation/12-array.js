e6055660f518350406c93b1a7c02d985f996b705
Butawantemi <japhetbuta@gmail.com>
2024-08-26 19:05:53 +0300

    Data manipulation

---
+++ 0x03-ES6_data_manipulation/12-array.js
@@ -0,0 +1,7 @@
+// An Array
+const citys = ['Dodoma', 'Dar es Salaam', 'Mwanza', 'Arusha'];
+
+// Iterate over the array and output each elements.
+for (const city of citys) {
+  console.log(city);
+}
