6d9f6d3c69c642f5d19039174d7fe942ea354a04
Butawantemi <japhetbuta@gmail.com>
2024-08-26 19:24:55 +0300

    Data manipulation

--- 0x03-ES6_data_manipulation/14-array.js
+++ 0x03-ES6_data_manipulation/14-array.js
@@ -1,4 +1,6 @@
-// An Array.
+// An Arrays.
 const brands = ['Apple', 'Samsung', 'Huawei'];
+const moreBrands = ['Sony', 'Nokia', 'LG'];
 
 // Concatenate two arrays.
+const allBrands = brands.concat(moreBrands);
