7f30418c2b4a0e0d39f5471c95de93f8dbf81478
Butawantemi <japhetbuta@gmail.com>
2024-08-26 18:11:31 +0300

    Data manipulation

---
+++ 0x03-ES6_data_manipulation/5-array.js
@@ -0,0 +1,9 @@
+// An Array
+const city = ['Dodoma', 'Dar es Salaam', 'Mwanza'];
+
+// Remove the last element from an array.
+const lastElement = city.pop();
+
+// Output the array and the removed element.
+console.log(city);
+console.log(lastElement);
