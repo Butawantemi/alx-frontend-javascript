9304e493fcc4016127a2f0c0593f721e35b32c79
Butawantemi <japhetbuta@gmail.com>
2024-08-26 18:17:08 +0300

    Data manipulation

--- 0x03-ES6_data_manipulation/6-array.js
+++ 0x03-ES6_data_manipulation/6-array.js
@@ -1,4 +1,10 @@
 // An Array
 const city = ['Dodoma', 'Dar es Salaam', 'Mwanza', 'Arusha'];
+const start = -2;
 
 // Remove maltiple elements from the end of an array.
+const removedElements = city.splice(start);
+
+// output the array and the removed elements.
+console.log(city);
+console.log(removedElements);
