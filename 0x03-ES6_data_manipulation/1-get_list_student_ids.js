d7d96466b32fec9d15233363c6c311ffca428170
Butawantemi <japhetbuta@gmail.com>
2024-08-30 15:09:06 +0300

    Data manipulation

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -2,5 +2,5 @@ export default function getListStudentIds(getListStudents) {
   if (!Array.isArray(getListStudents)) {
     return [];
   }
-  getListStudents.map(students => (students.id));
+  return getListStudents.map((students) => (students.id));
 }
