223b1973a83de623adcb1c2dd77a65b66722602a
Butawantemi <japhetbuta@gmail.com>
2024-08-30 21:32:47 +0300

    Data manipulation

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,3 +1,9 @@
 export default function createInt8TypedArray(length, postion, value) {
-    if ()
+  if (length >= postion) {
+    throw new Error('Position outside range');
+  }
+  const buffer = new ArrayBuffer(length);
+  const int8View = new Int8Array(buffer);
+  int8View[postion] = value;
+  return int8View;
 }
