b7616a509f39f89d35f32564f07d80b67d2bd3cc
Butawantemi <japhetbuta@gmail.com>
2024-08-30 20:51:09 +0300

    Data manipulation

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,7 +1,9 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
-  return students.filter((student) => student.location === city).map((student) => {
-    const newGrade = newGrades.filter((studentId) => studentId === student.id);
-    const grade = newGrade.length > 0 ? newGrade[0].grade : 'N/A';
-    return { ...student, grade };
-  });
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const newGrade = newGrades.filter((studentId) => studentId === student.id);
+      const { grade = 'N/A' } = newGrade.length > 0 ? newGrade[0] : {};
+      return { ...student, grade };
+    });
 }
