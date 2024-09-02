e4cd58fa099b107be2124920b1df59998842e7fb
Butawantemi <japhetbuta@gmail.com>
2024-09-02 20:23:39 +0300

    Data manipulation

--- 0x03-ES6_data_manipulation/Arrays/102-array.js
+++ 0x03-ES6_data_manipulation/Arrays/102-array.js
@@ -1,7 +1,7 @@
 const buffer = new ArrayBuffer(8);
 const uint8 = new Uint8Array(buffer);
 
-uint8.set([1, 2, 3, 4, 5, 6]);
+uint8.set([228, 189, 160, 229, 165, 189]);
 
 const text = new TextDecoder().decode(uint8);
 
