03ceb0a0526c68785f63bf8b453d4319d7ecd937
Butawantemi <japhetbuta@gmail.com>
2024-09-02 20:31:21 +0300

    Data manipulation

--- 0x03-ES6_data_manipulation/Arrays/101-array.js
+++ 0x03-ES6_data_manipulation/Arrays/101-array.js
@@ -1,4 +1,6 @@
 const typeArray = new Uint8Array([1, 2, 3, 4, 5]);
 
 const normalArray = Array.from(typeArray);
+const normalArray2 = [...typeArray];
 console.log(normalArray);
+console.log(normalArray2);
