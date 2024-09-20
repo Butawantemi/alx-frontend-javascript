ae3b1333b453c389cc31a875c46cd6f4ca8fcee4
Butawantemi <japhetbuta@gmail.com>
2024-09-20 16:51:30 +0300

    Data manipulation

--- 0x03-ES6_data_manipulation/set.js
+++ 0x03-ES6_data_manipulation/set.js
@@ -2,9 +2,11 @@
 const mySet1 = new Set();
 
 // Add values to the set.
-mySet1.add(1); // Set(1) { 1 }
-mySet1.add(5); // Set(2) { 1, 5 }
-mySet1.add(5); // Set(2) { 1, 5 }
-mySet1.add('some text'); // Set(3) { 1, 5, 'some text' }
+mySet1.add(1);
+mySet1.add(5);
+mySet1.add(5);
+mySet1.add('some text');
 const o = { a: 1, b: 2 };
 mySet1.add(o);
+
+console.log(mySet1);
