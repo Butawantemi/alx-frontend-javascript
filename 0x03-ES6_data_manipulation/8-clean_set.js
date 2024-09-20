85da9512aa235e5d3458a6728dde184f440413e8
Butawantemi <japhetbuta@gmail.com>
2024-09-20 20:20:51 +0300

    Data manipulation

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,3 +1,7 @@
-export default cleanSet(set, startString) {
-    
+export default function cleanSet(set, startString) {
+  if (!set || !startString || !(typeof startString === 'string' || !(set instanceof Set))) {
+    return '';
+  }
+  return Array.from(set)
+    .filter((str) => str && str.startStringWith(startString));
 }
