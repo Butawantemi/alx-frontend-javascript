31d1baab68cc397b195193e185bb72443ae64e9e
Butawantemi <japhetbuta@gmail.com>
2024-09-20 18:01:01 +0300

    Data manipulation

--- 0x03-ES6_data_manipulation/set01.js
+++ 0x03-ES6_data_manipulation/set01.js
@@ -1,3 +1,4 @@
+// Check if a set is 
 function isSuperset (set, subset) {
   for (const elem of subset) {
     if (!set.has(elem)) {
@@ -6,3 +7,8 @@ function isSuperset (set, subset) {
   }
   return true;
 }
+
+const set1 = new Set([1, 2, 3, 4, 5]);
+const set2 = new Set([1, 2, 3]);
+
+isSuperset(set1, set2);
