623f5e5e9e98a3084b81f5015c0ad719ffc1cb3c
Butawantemi <japhetbuta@gmail.com>
2024-09-20 16:54:14 +0300

    Data manipulation

--- 0x03-ES6_data_manipulation/set.js
+++ 0x03-ES6_data_manipulation/set.js
@@ -9,4 +9,10 @@ mySet1.add('some text');
 const o = { a: 1, b: 2 };
 mySet1.add(o);
 
-console.log(mySet1);
+// If the value already exists in the set.
+mySet1.has(1); // true
+mySet1.has(3); // false, since 3 has not been added to the set
+mySet1.has(5); // true
+mySet1.has(Math.sqrt(25)); // true
+mySet1.has('Some Text'.toLowerCase()); // true
+mySet1.has(o);
