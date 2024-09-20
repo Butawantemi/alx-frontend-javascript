a33997b418c2e19a72f263fe3cb4eef2d42c7e24
Butawantemi <japhetbuta@gmail.com>
2024-09-20 19:34:41 +0300

    Data manipulation

--- 0x03-ES6_data_manipulation/map.js
+++ 0x03-ES6_data_manipulation/map.js
@@ -3,3 +3,6 @@ const contacts = new Map();
 
 // Add a contact.
 contacts.set('Jessie', { phone: '213-555-1234', adress: '123 N 1st Ave' });
+contacts.set('Hilary', { phone: '617-555-4321', address: '321 S 2nd St' });
+
+console.log(contacts);
