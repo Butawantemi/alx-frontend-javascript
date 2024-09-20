ff08805c899028fb114bcaaf3ca9ee3ba74d0c8b
Butawantemi <japhetbuta@gmail.com>
2024-09-20 16:49:50 +0300

    Data manipulation

--- 0x03-ES6_data_manipulation/set.js
+++ 0x03-ES6_data_manipulation/set.js
@@ -1,5 +1,10 @@
 // Set data structure.
-const set01 = new Set();
+const mySet1 = new Set();
 
 // Add values to the set.
-set01.add(1);
+mySet1.add(1); // Set(1) { 1 }
+mySet1.add(5); // Set(2) { 1, 5 }
+mySet1.add(5); // Set(2) { 1, 5 }
+mySet1.add('some text'); // Set(3) { 1, 5, 'some text' }
+const o = { a: 1, b: 2 };
+mySet1.add(o);
