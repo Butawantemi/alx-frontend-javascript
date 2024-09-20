1e82436db1d6f0d8fc6a4da6ec0ec1774cc78ef8
Butawantemi <japhetbuta@gmail.com>
2024-09-20 20:08:31 +0300

    Data manipulation

--- 0x03-ES6_data_manipulation/map01.js
+++ 0x03-ES6_data_manipulation/map01.js
@@ -1,7 +1,11 @@
 // Map
 const myMap = new Map();
 
+// Add some key-value pairs.
 myMap.set(1, 'one');
 myMap.set(2, 'two');
 
-
+// iterate over the map.
+for (const [key, value] of myMap) {
+  console.log(`${key} = ${value}`);
+}
