ef1fee5fbd831e6ae86cd6ea547afd5a78fda4e4
Butawantemi <japhetbuta@gmail.com>
2024-09-21 18:50:28 +0300

    Data manipulation

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -4,7 +4,7 @@ export default function updateUniqueItems() {
   const map = groceriesList();
   for (const [key, value] of map) {
     if (value === 1) {
-        map.set(key, 100);
+      map.set(key, 100);
     }
   }
   return map;
