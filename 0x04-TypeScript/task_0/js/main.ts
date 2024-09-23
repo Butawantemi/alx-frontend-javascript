8e08e592496e37f1f3cc6bb658341e255b66edc5
Butawantemi <japhetbuta@gmail.com>
2024-09-23 13:32:26 +0300

    typescript

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -4,3 +4,19 @@ interface Student {
     age: number;
     location: string;
 }
+
+const student1: Student = {
+    firstName: 'Japhet',
+    lastName: 'Paul',
+    age: 23,
+    location: 'Dodoma'
+}
+
+const student2: Student = {
+    firstName: 'Jovin',
+    lastName: 'Paul',
+    age: 11,
+    location: 'Mwanza'
+}
+
+const studentsList: Array<Student> = [student1, student2];
