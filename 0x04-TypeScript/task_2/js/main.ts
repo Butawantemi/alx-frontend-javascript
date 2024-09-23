2a4b8d2f189f795235cb8a1ac5e3471421217deb
Butawantemi <japhetbuta@gmail.com>
2024-09-23 17:51:21 +0300

    typescript

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -38,3 +38,14 @@ class Teacher implements TeacherInterface {
     }
 }
 
+const createEmployee = (salary: number | string):
+    Director | Teacher => {
+        if (typeof salary === 'number' && salary < 500) {
+            return new Teacher();
+        } else {
+            return new Director();
+        }
+    }
+
+
+
