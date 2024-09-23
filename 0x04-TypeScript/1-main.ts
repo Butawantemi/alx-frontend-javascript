6e7efff46e6b289ae59ad6a7316c360c7fb21282
Butawantemi <japhetbuta@gmail.com>
2024-09-23 10:32:06 +0300

    typescript

---
+++ 0x04-TypeScript/1-main.ts
@@ -0,0 +1,18 @@
+interface User {
+    name: string;
+    id: number;
+}
+
+class UserAccount {
+    name: string;
+    id: number;
+
+    constructor(name: string, id: number) {
+        this.name = name;
+        this.id = id;
+    }
+}
+
+const user = new UserAccount("Japhet", 3);
+console.log(user);
+
