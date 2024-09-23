1f4f1ff1b0f23bc8b84e2ac991942f37d7c1cafb
Butawantemi <japhetbuta@gmail.com>
2024-09-23 13:40:13 +0300

    typescript

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -20,3 +20,17 @@ const student2: Student = {
 }
 
 const studentsList: Array<Student> = [student1, student2];
+
+const table = document.createElement('table');
+
+studentsList.forEach((student: Student) => {
+    const row = table.insertRow();
+    const cell1 = row.insertCell(0);
+    const cell2 = row.insertCell(1);
+
+    cell1.innerHTML = student.firstName;
+    cell2.innerHTML = student.lastName;
+})
+
+document.body.appendChild(table);
+
