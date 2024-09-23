2376e2a7a88f8c5bd2482886742877ca0ee065db
Butawantemi <japhetbuta@gmail.com>
2024-09-23 18:02:21 +0300

    typescript

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -59,5 +59,15 @@ const executeWork = (employee: Director | Teacher): string => {
     }
 }
 
+type Subjects = 'Math' | 'History';
+
+const teacherClass = (todayClass: Subjects): string => {
+    if (todayClass === 'Math') {
+        return 'Teaching Math';
+    } else {
+        return 'Teaching History';
+    }
+}
+
 
 
