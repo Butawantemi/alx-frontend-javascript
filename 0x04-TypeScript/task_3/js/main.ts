decf5ec0a780f16c184b98adb2e361f2b0dd7622
Butawantemi <japhetbuta@gmail.com>
2024-09-24 11:23:26 +0300

    typescript

---
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -0,0 +1,13 @@
+import { rowId, rowElement } from './interfaces.ts';
+import * as CRUD from './crud.js';
+
+const row: rowElement = {
+    firstName: 'Guillaume',
+    lastName: 'Salva'
+}
+
+const newRowId: rowId = CRUD.insertRow(row);
+const updatedRow: rowElement = { age: 23, ...row };
+
+CRUD.updatedRow(newRowId, updatedRow);
+CRUD.deleteRow(newRowId);
