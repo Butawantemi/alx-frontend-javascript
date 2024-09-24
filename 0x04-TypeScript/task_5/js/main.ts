56bd6eeb992f2602ec150781fd2192f1af6643d5
Butawantemi <japhetbuta@gmail.com>
2024-09-24 13:55:20 +0300

    typescript

--- 0x04-TypeScript/task_5/js/main.ts
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -11,3 +11,7 @@ interface manorCredits {
 const sumMajorCredits = (subject1: majorCredits, subject2: majorCredits) => {
     return subject1.credit + subject2.credit;
 }
+
+const sumManorCredits = (subject1: manorCredits, subject2: manorCredits) => {
+    return subject1.credit + subject2.credit;
+}
