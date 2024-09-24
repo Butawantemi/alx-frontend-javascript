8c387b4ab6ad12e307e1393eac9819ba96551060
Butawantemi <japhetbuta@gmail.com>
2024-09-24 12:45:53 +0300

    typescript

---
+++ 0x04-TypeScript/task_4/js/Java.ts
@@ -0,0 +1,21 @@
+/// <reference path='Teacher.ts' />
+
+namespace Subjects {
+    export interface Teacher {
+        experienceTeachingJava?: number;
+    }
+
+    export class Java extends Subject {
+        getRequirements(): string {
+            return 'Here is the list of requirements for Java';
+        }
+
+        getAvailableTeacher(): string {
+            if (this.teacher.experienceTeachingJava === undefined) {
+                return 'No available teacher';
+            } else {
+                return `Available Teacher: ${this.teacher.firstName}`;
+            }
+        }
+    }
+}
