2d428b43df7a59ecaaa31024d71024207cde29c8
Butawantemi <japhetbuta@gmail.com>
2024-09-24 12:28:29 +0300

    typescript

--- 0x04-TypeScript/task_4/js/Cpp.ts
+++ 0x04-TypeScript/task_4/js/Cpp.ts
@@ -4,18 +4,18 @@ namespace Subjects {
     export interface Teacher {
         experienceTeachingC?: number;
     }
-}
 
-export class Cpp extends Subject {
-    getRequirement(): string {
-        return 'Here is the list of requirements for Cpp';
-    }
-
-    getAvailableTeacher(): string {
-        if (this.teacher.experienceTeachingC === undefined) {
-            return 'No available teacher';
-        } else {
-            return `Available Teacher: ${this.teacher.firstName}`;
+    export class Cpp extends Subject {
+        getRequirement(): string {
+            return 'Here is the list of requirements for Cpp';
+        }
+    
+        getAvailableTeacher(): string {
+            if (this.teacher.experienceTeachingC === undefined) {
+                return 'No available teacher';
+            } else {
+                return `Available Teacher: ${this.teacher.firstName}`;
+            }
         }
     }
 }
