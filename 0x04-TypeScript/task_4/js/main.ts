7978853707bcf20340ebda6b646f40a746e0b132
Butawantemi <japhetbuta@gmail.com>
2024-09-24 13:28:25 +0300

    typescript

---
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -0,0 +1,10 @@
+/// <reference path='./subjects/Teacher.ts' />
+
+export const cpp = new Subjects.Cpp();
+export const java = new Subjects.Java();
+export const react = new Subjects.React();
+export const cTeacher: Subjects.Teacher = {
+    firstName: 'Japhet',
+    lastName: 'Paul',
+    experienceTeachingC: 10
+}
