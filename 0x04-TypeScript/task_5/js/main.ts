08796eec92891ceb6f9862812ee5c1276f8a7821
Butawantemi <japhetbuta@gmail.com>
2024-09-25 10:10:46 +0300

    typescript

--- 0x04-TypeScript/task_5/js/main.ts
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -1,17 +1,21 @@
+// Interface for the object of the type 'majorCredits'
 interface majorCredits {
     credit: number;
     brand: 'major';
 }
 
+// Interface for the object of type 'manorCredits'
 interface manorCredits {
     credit: number;
     brand: 'manor';
 }
 
+// Function that sums the credits of two objects of type 'majorCredits'
 const sumMajorCredits = (subject1: majorCredits, subject2: majorCredits) => {
     return subject1.credit + subject2.credit;
 }
 
+// Function that sums the credits of two objects of type 'manorCredits'
 const sumManorCredits = (subject1: manorCredits, subject2: manorCredits) => {
     return subject1.credit + subject2.credit;
 }
