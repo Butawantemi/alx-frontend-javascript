6d26d421c1208ca33ed1f0063dc30f5fb55214bc
Butawantemi <japhetbuta@gmail.com>
2024-09-25 10:28:58 +0300

    typescript

--- 0x04-TypeScript/task_4/js/subjects/Java.ts
+++ 0x04-TypeScript/task_4/js/subjects/Java.ts
@@ -1,10 +1,12 @@
 /// <reference path='Teacher.ts' />
 
+// Namespace Subjects with a new interface Teacher
 namespace Subjects {
     export interface Teacher {
         experienceTeachingJava?: number;
     }
-
+    
+    // Class Java that extends Subject.
     export class Java extends Subject {
         getRequirements(): string {
             return 'Here is the list of requirements for Java';
